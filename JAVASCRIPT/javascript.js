07161180f2872ad259a6c7271b4cbf09ffa06f67
Alekhya <96313151+Alekhyadantuluri@users.noreply.github.com>
2023-08-19 09:43:09 +0530

    Add files via upload

---
+++ JAVASCRIPT/javascript.js
@@ -0,0 +1,6 @@
+// image
+// document.getElementById('img1').src ="project-2.jpg";
+// // document.getElementById('img1').style.width ="100";
+// // document.getElementById('img1').style.height ="100";
+
+// document.getElementById("img1").style.border ="4px double pink";
